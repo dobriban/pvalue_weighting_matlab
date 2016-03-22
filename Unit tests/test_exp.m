e7cfbc7351e1192c4f855d3c4c28b73aa2fc85ad
Edgar Dobriban <dobriban@stanford.edu>
2016-03-22 16:25:31 -0700

    exp weights plots

--- Unit tests/test_exp.m
+++ Unit tests/test_exp.m
@@ -3,11 +3,36 @@
 cd('C:/Git/pvalue_weighting_matlab/Unit tests')
 addpath '../Code'
 
-%%
+%% negative means
 J = 200;
 mu = -abs(randn(J,1));
 beta = 4;
 q = 0.01; %0.05;
 w = exp_weights(mu,beta,q);
+figure,
 plot(mu,w,'*')
-
+filename = 'exp_weights_neg.png';
+saveas(gcf, filename,'png');
+fprintf(['Saved Results to ' filename '\n']);
+%% any means
+J = 200;
+mu = randn(J,1);
+beta = 4;
+q = 0.01; %0.05;
+w = exp_weights(mu,beta,q);
+figure,
+plot(mu,w,'*')
+filename = 'exp_weights.png';
+saveas(gcf, filename,'png');
+fprintf(['Saved Results to ' filename '\n']);
+%% any means - bounded
+J = 200;
+mu = randn(J,1);
+beta = 4;
+q = 0.05;
+w = exp_weights(mu,beta,q);
+figure,
+plot(mu,w,'*')
+filename = 'exp_weights_bdd.png';
+saveas(gcf, filename,'png');
+fprintf(['Saved Results to ' filename '\n']);
