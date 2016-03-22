e807c314f7511c8752e8887f91d87a8727e907bf
Edgar Dobriban <dobriban@stanford.edu>
2016-03-22 16:31:24 -0700

    fix bug in "Compare weighting methods" - exp weights
    The folder "Example10- Compare Weighting Methods - new" contains the
    updated results for the simulation comparing weighting methods. This
    uses the new version of the exp_weights function. It also fixed a bug in
    the simulation where the exp_weights were called with the argument
    abs(mu) instead of mu.
    
    The new results show that exp_weights are somewhat better than
    previously, but still not as good as Bayes weights.

---
+++ Examples/Example10- Compare Weighting Methods - new/power_study.m
@@ -0,0 +1,78 @@
+%% Study the power of various weighting schemes
+% depends on weighting_power_study
+codedir =  ['C:/Git/pvalue_weighting_matlab/Examples/Example10- Compare Weighting Methods'];
+cd(codedir);
+addpath '../../Code'
+addpath '../../Code/Helper Code'
+%% low noise absolute normal sigma
+
+J = 1e3; %number of tests
+rng(0);
+sigma = abs(randn(J,1));
+mu = randn(J,1);
+pfer = 10;
+pcer = pfer/J;
+q = pcer;
+
+[w,~,~,q_thresh] = bayes_weights(mu,sigma,pcer);
+%q_thresh  is 0.12;
+
+%power0 = @(w) sum(normcdf((norminv(pcer*w(mu<0))-mu(mu<0))./sqrt(sigma(mu<0).^2+1)))/J;
+power0 = @(w) sum(normcdf((norminv(min(pcer*w,1))-mu)./sqrt(sigma.^2+1)))/J;
+
+global_array = linspace(0.01,4,100);
+dispersion = global_array;
+for i=1:length(dispersion);
+    w = bayes_weights(mu,dispersion(i)*sigma,pcer);
+    power_reg(i) = power0(w);
+end
+
+beta = global_array;
+for i=1:length(beta);
+    w = exp_weights(mu, beta(i), pcer);
+    w = w/mean(w);
+    power_exp(i) = power0(w);
+end
+
+threshold = global_array;
+[mu_s,sort_index] = sort(mu);
+k = floor(J*q);
+topw = sort_index(1:k);
+
+for i=1:length(threshold);
+    ind = find(mu<-threshold(i));
+    w = zeros(J,1);
+    w(ind) = 1;
+    if mean(w)>0
+        w = w/mean(w);
+        if (max(w) > 1/q)
+            w = zeros(J,1);
+            w(topw) = 1/q;
+        end
+        power_top(i) = power0(w);
+    else
+        w = zeros(J,1);
+        w(topw) = 1/q;
+        power_top(i) = power0(w);  
+    end
+end
+
+power_unw = power0(ones(J,1))*ones(length(global_array),1);
+%%
+a = {'-','--',':','-.'};
+figure, hold on
+h = plot(global_array, power_unw,'linewidth',2); set(h,'LineStyle',a{1});
+h = plot(global_array, power_reg,'linewidth',2); set(h,'LineStyle',a{2});
+h = plot(global_array, power_exp,'linewidth',2); set(h,'LineStyle',a{3});
+h = plot(global_array, power_top,'linewidth',2); set(h,'LineStyle',a{4});
+ylim([0,0.2])
+h = legend('Unweighted','Bayes','Exponential','Filtering','location','Best');
+ylabel('Power');
+set(h,'FontSize',8);
+
+%%
+saveTightFigure(gcf,'compare_weighting_methods.pdf')
+%% plot
+filename = sprintf( './compare_weighting_methods_color.png');
+saveas(gcf, filename,'png');
+fprintf(['Saved Results to ' filename '\n']);
